ce9473a0af91cf9ae98d74f96a91b5a3d8ee5795
gitlawr <lawrleegle@gmail.com>
2023-03-06 16:20:41 +0800

    update webservice metadata

--- webservice/variables.tf
+++ webservice/variables.tf
@@ -16,21 +16,25 @@ variable "port" {
   description = "(Optional) Service ports"
   default = [80]
 }
-variable "advanced" {
-  type        = bool
-  description = "(Optional) Show advanced options"
-  default = false
-}
+
+# @group "Resources"
+# @label "CPU"
+# @options ["0.5","1","2"]
 variable "cpu" {
   type        = string
   description = "(Optional) CPU"
   default = "0.5"
 }
+
+# @group "Resources"
+# @label "Memory"
+# @options ["1Gi","2Gi","4Gi"]
 variable "memory" {
   type        = string
   description = "(Optional) Memory"
   default = "512Mi"
 }
+
 variable "env" {
   type        = map(string)
   description = "(Optional) Name and value pairs to set as the environment variables"
