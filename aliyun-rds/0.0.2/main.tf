9e86011c3480db031bd1ac8e63079404d5d1251e
pengjiang80 <peng.jiang@hotmail.com>
2023-04-18 14:42:51 +0800

    test-rds

---
+++ aliyun-rds/0.0.2/main.tf
@@ -0,0 +1,37 @@
+variable "creation" {
+  default = "Rds"
+}
+
+variable "name" {
+  default = "dbdatabasebasic"
+}
+
+data "alicloud_zones" "default" {
+  available_resource_creation = var.creation
+}
+
+resource "alicloud_vpc" "default" {
+  vpc_name       = var.name
+  cidr_block = "172.16.0.0/16"
+}
+
+resource "alicloud_vswitch" "default" {
+  vpc_id       = alicloud_vpc.default.id
+  cidr_block   = "172.16.0.0/24"
+  zone_id      = data.alicloud_zones.default.zones[0].id
+  vswitch_name = var.name
+}
+
+resource "alicloud_db_instance" "instance" {
+  engine           = "MySQL"
+  engine_version   = "5.6"
+  instance_type    = "rds.mysql.s1.small"
+  instance_storage = "10"
+  vswitch_id       = alicloud_vswitch.default.id
+  instance_name    = var.name
+}
+
+resource "alicloud_db_database" "default" {
+  instance_id = alicloud_db_instance.instance.id
+  name        = "tftestdatabase"
+}
