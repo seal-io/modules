848ec0709286b75ea1ae614bc1d55a9d9f538692
Peng Jiang <16434640+pengjiang80@users.noreply.github.com>
2023-04-19 13:39:33 +0800

    Update main.tf

--- awsrdsbeta/main.tf
+++ awsrdsbeta/main.tf
@@ -34,8 +34,8 @@ variable "db_subnet_group_name" {
   description = "Name of the DB subnet group"
 }
 
-variable "db_security_group_ids" {
-  description = "List of security group IDs to be associated with the RDS instance"
+variable "db_security_group_name" {
+  description = "Name of security group to be associated with the RDS instance"
 }
 
 variable "db_backup_retention_period" {
@@ -62,7 +62,7 @@ resource "aws_db_instance" "rds_instance" {
   username             = var.db_username
   password             = var.db_password
   db_subnet_group_name = var.db_subnet_group_name
-  vpc_security_group_ids = var.db_security_group_ids
+  vpc_security_group_name = var.db_security_group_name
   backup_retention_period = var.db_backup_retention_period
   multi_az             = var.db_multi_az
   tags                 = var.db_tags
