0a8141f129d15864968c3ad51d7f943af122eb82
linyinli <liyinlin926@gmail.com>
2023-04-20 15:34:33 +0800

    Remove outputs.tf temporary

--- microservices/0.0.1/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "frontend_external_endpoint" {
-    description = "Access URL" 
-    value       = "http://${data.kubernetes_service.service.status.load_balancer.0.ingress.0.hostname}:${data.kubernetes_service.service.spec.ports.0.node_port}"
-}
