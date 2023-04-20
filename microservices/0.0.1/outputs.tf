296c42c213d3538d7b3a10e0ca2876edb1afc9c3
linyinli <liyinlin926@gmail.com>
2023-04-20 19:39:49 +0800

    Update outputs.tf

--- microservices/0.0.1/outputs.tf
+++ microservices/0.0.1/outputs.tf
@@ -1,4 +1,4 @@
 output "frontend_external_endpoint" {
     description = "Online Boutique frontend access URL" 
-    value       = "http://${tolist(data.kubernetes_service.frontend_service.spec.0.external_ips)[0]}"
+    value       = "http://${tolist(data.kubernetes_service.frontend_service.spec.0.external_ips)[0]}:${data.kubernetes_service.frontend_service.port.node_port}"
 }
