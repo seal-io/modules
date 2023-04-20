1f54ba413e3a0a855952611307a9b5ed978d6f68
pengjiang80 <peng.jiang@hotmail.com>
2023-04-20 15:11:25 +0800

    update

--- aliyun-serverless-rds/0.0.1/main.tf
+++ aliyun-serverless-rds/0.0.1/main.tf
@@ -1,19 +1,3 @@
-variable "instance_name" {
-  description = "数据库实例名称"
-  default = "seal-demo-serverless"
-}
-
-variable "db_name" {
-  description = "数据库名称"
-  default = "demodb"
-}
-
-variable "allocate_public_connection" {
-  decription = "是否开通数据库互联网访问"
-  type    = bool
-  default = true
-}
-
 data "alicloud_db_zones" "example" {
   engine                   = "MySQL"
   engine_version           = "8.0"
