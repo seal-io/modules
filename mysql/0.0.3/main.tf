cd40bb5dda2b0328519cc84b764e29fb1ece52e2
gitlawr <lawrleegle@gmail.com>
2023-06-12 17:48:10 +0800

    refactor: update metadata vars

---
+++ mysql/0.0.3/main.tf
@@ -0,0 +1,51 @@
+resource "random_string" "password" {
+  length  = 16
+  special = false
+}
+
+resource "helm_release" "mysql" {
+  # disable wait for all resource be ready.
+  #
+  wait = false
+
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "mysql"
+
+  create_namespace = true
+  namespace        = local.namespace
+  name             = local.name
+
+  set {
+    name  = "fullnameOverride"
+    value = local.name
+  }
+  set {
+    name  = "auth.database"
+    value = var.database
+  }
+  set {
+    name  = "auth.username"
+    value = var.username
+  }
+  set {
+    name  = "auth.password"
+    value = coalesce(var.password, random_string.password.result)
+  }
+  set {
+    name  = "initdbScripts.init_script\\.sql"
+    value = var.init_db_script
+  }
+}
+
+data "kubernetes_service" "mysql_service" {
+  depends_on = [helm_release.mysql]
+  metadata {
+    name      = local.name
+    namespace = local.namespace
+  }
+}
+
+locals {
+  name      = "${var.seal_metadata_service_name}-mysql"
+  namespace = coalesce(var.namespace, "${var.seal_metadata_project_name}-${var.seal_metadata_environment_name}-${var.seal_metadata_service_name}")
+}
