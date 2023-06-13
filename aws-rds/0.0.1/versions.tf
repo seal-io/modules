043459d16de98cb672612dd32fec37ef83b35414
thxCode <thxcode0824@gmail.com>
2023-06-13 18:27:34 +0800

    feat: aws-rds module
    Signed-off-by: thxCode <thxcode0824@gmail.com>

---
+++ aws-rds/0.0.1/versions.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+
+    byteset = {
+      source = "seal-io/byteset"
+    }
+  }
+}
