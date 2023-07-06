c0b8667f91f2ba2bdc7685ed9beff86905f6fe59
michelia <michelia@seal.io>
2023-07-06 14:36:49 +0800

    refactor: support deploy source code

--- deploy-source-code/0.0.1/outputs.tf
+++ deploy-source-code/0.0.1/outputs.tf
@@ -1,3 +1,13 @@
+output "service_ip" {
+  description = "Service IP"
+  value       = data.kubernetes_service.service.spec.0.cluster_ip
+}
+
+output "ports" {
+  description = "Service Ports"
+  value       = var.ports
+}
+
 output "image" {
   description = "Built docker image name"
   value       = var.image
